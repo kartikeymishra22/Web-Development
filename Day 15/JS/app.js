0283e65870d71d0a27b8a3fa9593a742a9bdd0f1
Kartikey Mishra <kartikeymishra700@gmail.com>
2024-05-17 00:50:07 +0530

    commit

---
+++ Day 15/JS/app.js
@@ -0,0 +1,28 @@
+// let color = "red";
+
+// // Traffic ligths
+
+// if(color === "red"){
+//     console.log("Stop!");
+// }
+// if(color === "yellow"){
+//     console.log("Slow Down");
+// }
+// if(color === "green"){
+//     console.log("Go");
+// }
+
+let size = "S";
+
+if(size === "XL"){
+    console.log("Price is Rs.250");
+}
+else if(size === "L"){
+    console.log("Price is Rs.200");
+}
+else if(size === "M"){
+    console.log("Price is Rs.100");
+}
+else{
+    console.log("Price is Rs.50");
+}
---
+++ Day 16/Array/app.js
@@ -0,0 +1,29 @@
+// let student1 = "kartikey";
+// let student2 = "Atul";
+// let stiudent3 = "Rajat";
+
+// let students = ["kartikey", "Atul", "rajat"];
+
+// 
+
+// let num = 120;
+// if(num % 10 == 0){
+//     console.log("Good");
+// }
+// else{
+//     console.log("bad");
+// }
+
+// let name = prompt("Enter your Name");
+// let age = prompt('Enter your Age');
+
+// alert(`Your Name is ${name}, and Your Age is ${age}.`)
+
+let str = "Apple";
+
+if(str.length >= 5 && (str[0] == 'A' || atr[0] == 'a')){
+    console.log("Good");
+}
+else{
+    console.log("Bad");
+}
